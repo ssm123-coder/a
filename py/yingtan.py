9f0a73207e17157c12260deabb9c805e6d881acb
ssm123-coder <mafude123@gmail.com>
2026-09-20 00:34:13 +0800

    Update yingtan.py

--- py/yingtan.py
+++ py/yingtan.py
@@ -17,12 +17,11 @@ class Spider(Spider):
         'cmskey': 'wP5bvxoc3yv7FoBQENFZuAF0EUYr4LTy',
         'RawPlayUrl': 0,
         # ldmax 内置解析接口
-        'parse_api': 'https://niubi.69mini.com/api/?key=de8570d02b2e5181978a6c47a8eb4d91&url='
+        'parse_api': 'http://61.184.23.217:6163/api/index?parsesId=4&appid=10001&videoUrl='
     }
     WEB_PARSES = [
         'http://yecao.icu/api/index?parsesId=3&appid=10000&videoUrl=',
         'http://61.184.23.217:6163/api/index?parsesId=4&appid=10001&videoUrl=',
-        'http://125.208.22.184:6019/api/index?parsesId=550266&appid=10000&videoUrl=',
     ]
 
     def init(self, extend=''):
