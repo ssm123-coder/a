fb7df3617bd817a66d03eb98e8904d072893a8fd
ssm123-coder <mafude123@gmail.com>
2026-09-20 00:20:32 +0800

    Update yingtan.py

--- py/yingtan.py
+++ py/yingtan.py
@@ -17,8 +17,12 @@ class Spider(Spider):
         'cmskey': 'wP5bvxoc3yv7FoBQENFZuAF0EUYr4LTy',
         'RawPlayUrl': 0,
         # ldmax 内置解析接口
-        'parse_api': 'http://61.184.23.217:6163/api/index?parsesId=4&appid=10001&videoUrl='
+        'parse_api': 'http://yecao.icu/api/index?parsesId=3&appid=10000&videoUrl='
     }
+    WEB_PARSES = [
+        'http://yecao.icu/api/index?parsesId=3&appid=10000&videoUrl=',
+        'http://61.184.23.217:6163/api/index?parsesId=4&appid=10001&videoUrl=',
+    ]
 
     def init(self, extend=''):
         self.host = self.FIXED_CONFIG['host']
